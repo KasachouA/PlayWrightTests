e8d3a3e4c3c676138ea1eefbd13edd9b7dfaac42
Andrei Kasachou <andrei_kasachou@epam.com>
2024-10-09 18:22:22 +0300

    config added

---
+++ TestProject/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+
+namespace CoreTestProject
+{
+    public static class Configuration
+    {
+
+        private static string GetValue(string key)
+        {
+            var environmentVariable = Environment.GetEnvironmentVariable(key);
+            var configValue = ConfigurationManager.AppSettings.Get(key);
+            return environmentVariable ?? configValue;
+        }
+    }
+}
